6feaf526984c0d1c2e0a9a7b20d4524f9309a670
CheeMinHao <cheemh3456@gmail.com>
2023-01-15 13:47:06 +0800

    website version

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
 import qrcode
-import datetime
-# Link for website
-input_data = "https://forms.gle/Nran4R1QjpNuaFYB8"
-#Creating an instance of qrcode
-qr = qrcode.QRCode(
-        version=1,
-        box_size=10,
-        border=5)
-qr.add_data(input_data)
-qr.make(fit=True)
-img = qr.make_image(fill='black', back_color='white')
+import streamlit as st
+from io import BytesIO
 
-img.save(f'qrcodes/{datetime.datetime.now()}.png')
+
+def main():
+        st.title("QR Code Generator")
+        url_input = st.text_input("Please insert your URL link here")
+        qr_name = st.text_input("Please insert your desired file name")
+        generate_qr = st.button("Generate QR Code")
+        if generate_qr:
+                create_qr_code(url_input, qr_name)
+
+def create_qr_code(url_input, qr_name):
+
+        enable_download = False
+
+        #Creating an instance of qrcode
+        qr = qrcode.QRCode(
+                version=1,
+                box_size=10,
+                border=5)
+        try:
+                qr.add_data(url_input)
+                qr.make(fit=True)
+                img = qr.make_image(fill='black', back_color='white')
+        except:
+                st.error("Invalid Link provided, please try again.")
+
+        enable_download = True
+        if enable_download:
+                buf = BytesIO()
+                img.save(buf, format="PNG")
+                byte_im = buf.getvalue()
+                st.image(byte_im, width=300)
+                st.download_button(label="Download Your QR Code", data=byte_im, file_name=f'{qr_name}.png', mime="image/png")
+
+if __name__ == "__main__":
+        main()
